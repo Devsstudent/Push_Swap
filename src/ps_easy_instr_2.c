8f5e7deca8929b91bfa72f50daee73838cb382d8
Orson DESSEIN <odessein@e1r1p12.clusters.42paris.fr>
2022-07-18 23:54:35 +0200

    Handle the 1 2 3 4 0 case : )

--- src/ps_easy_instr_2.c
+++ src/ps_easy_instr_2.c
@@ -6,10 +6,23 @@
 /*   By: odessein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 22:39:36 by odessein          #+#    #+#             */
-/*   Updated: 2022/07/08 22:39:58 by odessein         ###   ########.fr       */
+/*   Updated: 2022/07/18 23:52:40 by odessein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ps.h"
+
+static t_bool	ps_do_instruction(t_list **stack_a, t_list **stack_b, int i)
+{
+	int		j;
+
+	j = -1;
+	while (++j < i && i < 3)
+		ps_ra(stack_a);
+	while (++j < i && i >= 3)
+		ps_rra(stack_a);
+	ps_pb(stack_a, stack_b);
+		return (TRUE);
+}
 
 void	ps_five(t_list **stack_a, t_list **stack_b)
 {
@@ -27,23 +40,20 @@ void	ps_push_min(t_list **stack_a, t_list **stack_b)
 	t_list	*min;
 	t_list	*buff;
 	int		i;
-	int		j;
 
 	i = 0;
-	j = -1;
 	min = ps_find_min(stack_a);
 	buff = *stack_a;
 	while (buff != NULL)
 	{
-		if (buff == min)
+		if (buff == min && buff->next == NULL)
 		{
-			while (++j < i && i < 3)
-				ps_ra(stack_a);
-			while (++j < i && i >= 3)
-				ps_rra(stack_a);
+			ps_rra(stack_a);
 			ps_pb(stack_a, stack_b);
-			return ;
 		}
+		else if (buff == min)
+			if (ps_do_instruction(stack_a, stack_b, i))
+				return ;
 		buff = buff->next;
 		i++;
 	}
